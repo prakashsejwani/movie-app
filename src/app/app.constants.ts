5829f7723649e2ccd4eeec312eabe390b6cce522
Prakash Sejwani <prakash.sejwani@kalpavruksh.com>
2017-09-23 10:47:38 +0530

    Add getPopularMovies method in movie service

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Configuration {
   public Server = 'https://api.themoviedb.org/';
-  public ApiUrl = 'discover/movie';
+  public ApiUrl = '3/discover/movie';
   public ServerWithApiUrl = this.Server + this.ApiUrl;
 }
--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import { Configuration } from './app.constants';
+import { Movie } from './movie';
+import { environment } from '../environments/environment';
+
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class MovieService {
+  private params = {'api_key': environment.apiKey, 'sort_by': 'popularity.desc'};
+  private actionUrl: string;
 
-  constructor() { }
+  constructor(private http: Http, private _configuration: Configuration) {
+    this.actionUrl = _configuration.ServerWithApiUrl;
+  }
+
+  getPopularMovies(): Promise<Movie[]> {
+    return this.http.get(this.actionUrl, {params: this.params})
+               .toPromise()
+               .then(response => response.json().results as Movie[])
+               .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
 
 }
